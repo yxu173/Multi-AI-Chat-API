9b657b838f864f72571dd08f3c0547e4daf00be4
yxu <mohamedsamir177@outlook.com>
2025-03-22 10:14:38 +0000

    Remove MaxInputTokens and MaxOutputTokens from UserAiModelSettings and add AiAgentId to ChatSessions

---
+++ src/Infrastructure/Database/Migrations/20250322052450_EditChatSession.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditChatSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxInputTokens",
+                table: "UserAiModelSettings");
+
+            migrationBuilder.DropColumn(
+                name: "MaxOutputTokens",
+                table: "UserAiModelSettings");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AiAgentId",
+                table: "ChatSessions",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AiAgentId",
+                table: "ChatSessions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxInputTokens",
+                table: "UserAiModelSettings",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxOutputTokens",
+                table: "UserAiModelSettings",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
