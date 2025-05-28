617b614063a81b6736c25c741123187d590121c6
yxu <mohamedsamir177@outlook.com>
2025-05-28 15:06:44 +0300

    Add columns to FileAttachments for processing metadata
    Added `ProcessedDataCacheKey` and `ProcessingStatus` columns to the `FileAttachments` table. These changes facilitate tracking processing state and caching associated data.

---
+++ src/Infrastructure/Database/Migrations/20250528113605_EditFileAttachment.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditFileAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProcessedDataCacheKey",
+                table: "FileAttachments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProcessingStatus",
+                table: "FileAttachments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessedDataCacheKey",
+                table: "FileAttachments");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessingStatus",
+                table: "FileAttachments");
+        }
+    }
+}
